77a65c702ed87d186d0d09f30edb6e6f67b00b71
GabrielPS <gabriel.pinto@aluno.cefet-rj.br>
2023-06-01 15:26:12 -0300

    Problema da leitura de operacao "resolvido".

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -3,18 +3,18 @@
 
 int main()
 {
-    float numero1, numero2;
+    int numero1, numero2;
     char operacao;
     printf("--Calculadora--\n");
 
-    printf("Digite o primeiro numero:\n");
-    scanf("%f", &numero1);
-
-    printf("Digite o segundo numero:\n");
-    scanf("%f", &numero2);
-
     printf("Digite o caractere correspondente a operacao que sera executada: \n+(adicao) \n-(subtracao) \n*(multiplicacao) \n/(divisao):\n");
     scanf("%c", &operacao);
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%d", &numero1);
+
+    printf("Digite o segundo numero:\n");
+    scanf("%d", &numero2);
 
     printf("A operacao selecionada foi: %c", operacao);
     return 0;
